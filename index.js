b9994ba187ea99e673da583e57daf5187ddbaec3
Rudi Persson <Rudipersson@gmail.com>
2018-04-01 11:14:02 +0100

    db added

--- index.js
+++ index.js
@@ -1,93 +1,93 @@
 const express = require('express');
 const app = express();
-// const MongoClient = require('mongodb').MongoClient;
-// const ObjectID = require('mongodb').ObjectID;
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
-// // Connect to MongoDb
-// const uri = "mongodb+srv://Rudi:dfvPZYENNbrntoXM@myfirstcluster-cvwrb.mongodb.net"
-// const connection = (closure) => {
-//     return MongoClient.connect(uri, (err, client) => {
-//         // const db = client.db('mean');
-//         // if (err) return console.log(err);
-//         if(err) throw err;
+// Connect to MongoDb
+const uri = "mongodb+srv://Rudi:dfvPZYENNbrntoXM@myfirstcluster-cvwrb.mongodb.net"
+const connection = (closure) => {
+    return MongoClient.connect(uri, (err, client) => {
+        // const db = client.db('mean');
+        // if (err) return console.log(err);
+        if(err) throw err;
 
-//         var db = client.db('mandatory');
+        var db = client.db('mandatory');
 
-//         // db.collection('users').findOne({}, function (findErr, result) {
-//         //     if (findErr) throw findErr;
-//         //     console.log(result.name);
-//         //     client.close();
-//         //   });
+        // db.collection('users').findOne({}, function (findErr, result) {
+        //     if (findErr) throw findErr;
+        //     console.log(result.name);
+        //     client.close();
+        //   });
         
-//         //if (err) return console.log(err);
+        //if (err) return console.log(err);
 
-//         closure(db);
-//     });
-// };
+        closure(db);
+    });
+};
 
 
 // Error handling
-// const sendError = (err, res) => {
-//     response.status = 501;
-//     response.message = typeof err == 'object' ? err.message : err;
-//     res.status(501).json(response);
-// };
+const sendError = (err, res) => {
+    response.status = 501;
+    response.message = typeof err == 'object' ? err.message : err;
+    res.status(501).json(response);
+};
 
 // // Response handling
-// let response = {
-//     status: 200,
-//     data: [],
-//     message: null
-// };
+let response = {
+    status: 200,
+    data: [],
+    message: null
+};
 
 
 // Get Bowlers
-// app.get('/api/bowlers', (req, res) => {
-//     connection((db) => {
-//         db.collection('Bowlers')
-//             .find()
-//             .toArray()
-//             .then((bowlers) => {
-//                 response.data = bowlers;
-//                 res.json(response);
-//             })
-//             .catch((err) => {
-//                 sendError(err, res);
-//             });
-//     });
-// });
+app.get('/api/bowlers', (req, res) => {
+    connection((db) => {
+        db.collection('Bowlers')
+            .find()
+            .toArray()
+            .then((bowlers) => {
+                response.data = bowlers;
+                res.json(response);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+});
 
 // Get Tournaments
-// app.get('/api/tournaments', (req, res) => {
-//     connection((db) => {
-//         db.collection('Tournaments')
-//             .find()
-//             .toArray()
-//             .then((tournaments) => {
-//                 response.data = tournaments;
-//                 res.json(response);
-//             })
-//             .catch((err) => {
-//                 sendError(err, res);
-//             });
-//     });
-// });
+app.get('/api/tournaments', (req, res) => {
+    connection((db) => {
+        db.collection('Tournaments')
+            .find()
+            .toArray()
+            .then((tournaments) => {
+                response.data = tournaments;
+                res.json(response);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+});
 
 // Get LVS
-// app.get('/api/lvs', (req, res) => {
-//     connection((db) => {
-//         db.collection('lvs')
-//             .find().limit(200)
-//             .toArray()
-//             .then((lvsData) => {
-//                 response.data = lvsData;
-//                 res.json(response);
-//             })
-//             .catch((err) => {
-//                 sendError(err, res);
-//             });
-//     });
-// });
+app.get('/api/lvs', (req, res) => {
+    connection((db) => {
+        db.collection('lvs')
+            .find().limit(200)
+            .toArray()
+            .then((lvsData) => {
+                response.data = lvsData;
+                res.json(response);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
+    });
+});
 
 const courses = [{
         id: 1,
