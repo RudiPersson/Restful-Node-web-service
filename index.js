33d043316628037e7cce3ead9e102bd33be27d29
Rudi Persson <Rudipersson@gmail.com>
2018-04-06 11:32:05 +0200

    cors

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 // Connect to MongoDb
 const uri = "mongodb+srv://Rudi:dfvPZYENNbrntoXM@myfirstcluster-cvwrb.mongodb.net"
 const connection = (closure) => {
