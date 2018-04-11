a2019270cd7a8a108e6a111f74d662511f9cd0e1
Rudi Persson <Rudipersson@gmail.com>
2018-04-11 09:42:04 +0200

    First commit, Deleted some unused objects and routes.

--- index.js
+++ index.js
@@ -98,34 +98,7 @@ app.get('/api/lvs', (req, res) => {
     });
 });
 
-const courses = [{
-        id: 1,
-        name: 'course 1'
-    },
-    {
-        id: 2,
-        name: 'course 2'
-    },
-    {
-        id: 3,
-        name: 'course 3'
-    }
-]
 
-app.get('/', (req, res) => {
-    res.send('hello world!!!!!');
-});
-
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-});
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id == parseInt(req.params.id))
-   if(!course) res.status(404).send('Course with the given ID was not found.')
-   res.send(course);
-});
 
 
 // PORT
